35bd13cd9c52613fda947c461f17c92e4d370c85
siddhusaravanan1 <siddhumyname@gmail.com>
2025-04-08 13:13:28 +0530

    added ATM setup and made adjustments with the test positions

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+love.graphics.setDefaultFilter("nearest", "nearest")
+
 local totalBalance = 10000
 local error = 0
 
@@ -45,7 +47,7 @@ function love.keypressed(key)
             showData = false
         end
     end
-    if key == "e" then
+    if key == "y" then
         if day1Debiters[currentIndex].balance >= day1Debiters[currentIndex].debtAmount and not day1Debiters[currentIndex].paid then
             if totalBalance >= day1Debiters[currentIndex].debtAmount then
                 totalBalance = totalBalance - day1Debiters[currentIndex].debtAmount
@@ -64,18 +66,37 @@ function love.keypressed(key)
             currentIndex = currentIndex + 1
         end
     end
+    if key == "n" then
+        if day1Debiters[currentIndex].balance >= day1Debiters[currentIndex].debtAmount and not day1Debiters[currentIndex].paid then
+            if totalBalance >= day1Debiters[currentIndex].debtAmount then
+                error = error + 1
+                currentIndex = currentIndex + 1
+            else
+                print("No amount")
+                currentIndex = currentIndex + 1
+            end
+        else
+            print("Insufficient Balance")
+            currentIndex = currentIndex + 1
+        end
+    end
+end
+function love.load()
+    credsSetup = love.graphics.newImage("sprites/CredsSetup.png")
 end
 
 function love.draw()
+    love.graphics.draw(credsSetup, 0, 200, nil, 5)
     love.graphics.print("TotalBalance: " .. totalBalance, 10, 10)
     love.graphics.print("ErrorCount: " .. error, 10, 25)
     if showData and day1Debiters[currentIndex] then
         local debtor = day1Debiters[currentIndex]
-        love.graphics.print("Name: " .. debtor.name, 100, 100)
-        love.graphics.print("Balance: " .. debtor.balance, 100, 130)
-        love.graphics.print("Debt Amount: " .. debtor.debtAmount, 100, 160)
-        love.graphics.print("Press [e] to see next", 100, 200)
+        love.graphics.print("Name: " .. debtor.name, 400, 300)
+        love.graphics.print("Balance: " .. debtor.balance, 400, 330)
+        love.graphics.print("Debt Amount: " .. debtor.debtAmount, 400, 360)
+        love.graphics.print("Press [Y] to accept", 400, 400)
+        love.graphics.print("Press [N] to reject", 400, 425)
     else
-        love.graphics.print("Press [Space] to start showing debtors", 100, 100)
+        love.graphics.print("Press [Space] to start showing debtors", 400, 300)
     end
 end
